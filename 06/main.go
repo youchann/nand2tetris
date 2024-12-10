292335dd92e3c7c4e4e4fd9f880b259482cb9def
Yutaro Hayashi <yutaro50@gmail.com>
2024-12-10 11:49:58 +0900

    refactor

--- 06/main.go
+++ 06/main.go
@@ -18,6 +18,47 @@ func getHackFilePath(asmPath string) string {
 	return filepath.Join(dir, hackName)
 }
 
+func assemble(content string) ([]string, error) {
+	var machineCode []string
+	p := parser.New(content)
+	for p.HasMoreLines() {
+		var instruction string
+		switch p.CommandType() {
+		case parser.A_INSTRUCTION:
+			instruction = code.Symbol(p.Symbol())
+		case parser.C_INSTRUCTION:
+			instruction = "111" + code.Comp(p.Comp()) + code.Dest(p.Dest()) + code.Jump(p.Jump())
+		case parser.L_INSTRUCTION:
+			// Lコマンドはマシン語を生成しない
+			p.Advance()
+			continue
+		}
+		if instruction != "" {
+			machineCode = append(machineCode, instruction)
+		}
+		p.Advance()
+	}
+
+	return machineCode, nil
+}
+
+func writeToFile(filepath string, instructions []string) error {
+	file, err := os.Create(filepath)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %v", err)
+	}
+	defer file.Close()
+
+	for _, instruction := range instructions {
+		_, err := file.WriteString(instruction + "\n")
+		if err != nil {
+			return fmt.Errorf("failed to write instruction to file: %v", err)
+		}
+	}
+
+	return nil
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: go run main.go [filename]")
@@ -29,38 +70,23 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error: File must have .asm extension\n")
 		os.Exit(1)
 	}
+
 	content, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
 		os.Exit(1)
 	}
 
-	resultFilePath := getHackFilePath(filename)
-	hackFile, err := os.Create(resultFilePath)
+	machineCode, err := assemble(string(content))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error creating output file: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error assembling code: %v\n", err)
 		os.Exit(1)
 	}
-	defer hackFile.Close()
 
-	p := parser.New(string(content))
-	for p.HasMoreLines() {
-		result := ""
-		switch p.CommandType() {
-		case parser.A_INSTRUCTION:
-			result = code.Symbol(p.Symbol()) + "\n"
-		case parser.C_INSTRUCTION:
-			result = "111" + code.Comp(p.Comp()) + code.Dest(p.Dest()) + code.Jump(p.Jump()) + "\n"
-		case parser.L_INSTRUCTION:
-		}
-
-		if result != "" {
-			_, err := hackFile.WriteString(result)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error writing to file: %v\n", err)
-				os.Exit(1)
-			}
-		}
-		p.Advance()
+	resultFilePath := getHackFilePath(filename)
+	err = writeToFile(resultFilePath, machineCode)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing output: %v\n", err)
+		os.Exit(1)
 	}
 }
